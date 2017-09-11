68dbaa2bc0762254a6714bbcb45ec66cc212338d
Eric W. Greene <eric@ericwgreene.com>
2017-09-11 13:41:56 -0400

    added state form fields demo

--- src/ts/components/color-tool.tsx
+++ src/ts/components/color-tool.tsx
@@ -6,7 +6,29 @@ interface ColorToolProps {
   colors: ColorItem[];
 }
 
-export class ColorTool extends React.Component<ColorToolProps, undefined> {
+interface ColorToolState {
+  newColorName: string;
+  newColorHexCode: string;
+}
+
+export class ColorTool extends React.Component<ColorToolProps, ColorToolState> {
+
+  public constructor(props: ColorToolProps) {
+    super(props);
+
+    this.state = {
+      newColorName: '',
+      newColorHexCode: '',
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  public onChange(e: any) {
+    this.setState({
+      [ e.currentTarget.name ]: e.currentTarget.value,
+    });
+  }
 
   public render() {
 
@@ -17,6 +39,18 @@ export class ColorTool extends React.Component<ColorToolProps, undefined> {
       <ul>
         {this.props.colors.map((color) => <li key={color.id}>{color.name}</li>)}
       </ul>
+      <form>
+        <div>
+          <label htmlFor="new-color-name-input">New Color Name:</label>
+          <input type="text" id="new-color-name-input" name="newColorName"
+            value={this.state.newColorName} onChange={this.onChange} />
+        </div>
+        <div>
+          <label htmlFor="new-color-hex-code-input">New Color HexCode:</label>
+          <input type="color" id="new-color-hex-code-input" name="newColorHexCode"
+            value={this.state.newColorHexCode} onChange={this.onChange} />
+        </div>
+      </form>
     </div>;
   }
 }
