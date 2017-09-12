c232b29e76b88b98e8e43a6ed0a6df9e00f50756
Eric W. Greene <eric@ericwgreene.com>
2017-09-12 08:55:07 -0400

    added stateless component

--- src/ts/components/tool-header.tsx
+++ src/ts/components/tool-header.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
 
-export class ToolHeader extends
-  React.Component<{ headerText: string }, undefined> {
+export const ToolHeader = (props: { headerText: string }) => {
 
-    public render() {
+  return <header>
+    <h1>{props.headerText}</h1>
+  </header>;
 
-      return <header>
-        <h1>{this.props.headerText}</h1>
-      </header>;
+};
 
-    }
+// export class ToolHeader extends
+// React.Component<{ headerText: string }> {
 
-}
+//   public render() {
+
+//     return <header>
+//       <h1>{this.props.headerText}</h1>
+//     </header>;
+
+//   }
+
+// }
